1d053bf7d7b0c87be7792e601bc5893ab663f346
VENOCKA <thomas.ruiz@ynov.com>
2023-01-19 16:55:04 +0100

    Recupe la localisation user et affichage MapView

--- src/screens/MapsScreen.tsx
+++ src/screens/MapsScreen.tsx
@@ -1,25 +1,47 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import React from 'react'
-import { Button, View, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Button, View, StyleSheet, Text } from 'react-native'
 import MapView from 'react-native-maps'
 import { StackNavigatorParams } from '../utils/router'
+import * as Location from 'expo-location';
 
 type Props = NativeStackScreenProps<StackNavigatorParams, 'Maps'>
 
 export const MapsScreen = ({ navigation } : Props) => {
 
+  const [localLatitude, setLatitude] = useState(0)
+  const [localLongitude, setLongitude] = useState(0)
+
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      const location2 = await Location.getCurrentPositionAsync({});
+      setLatitude(location2?.coords?.latitude)
+      setLongitude(location2?.coords?.longitude)
+    })();
+  }, []);
+
     return(
         <View style={styles.container}>
             <Button title="Go Home" onPress={ () => navigation.navigate('Home')} />
-            <MapView 
-                style={styles.map}
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                  }}
-            />
+            <MapView
+              style={styles.map}
+              initialRegion={{
+                latitude: localLatitude,
+                longitude: localLongitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+              }}
+            >
+
+            </MapView>
         </View>
     )
 }
